8d97455ecc4ce9634a11bb8be557a5c076c12998
Noah Hoffman <stacknoah99@gmail.com>
2022-09-07 16:12:42 -0600

    started outline of skill section

--- src/components/SkillSection/skill.js
+++ src/components/SkillSection/skill.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './skill.scss'
+import FrontEnd from '../../SVG/front-end.png'
+import BackEnd from '../../SVG/ux-design.png'
+import Tools from '../../SVG/management-service.png'
 
 function Skills() {
     return (
-        <div className="skillContainer">
+        <div className="skillSection">
             <section>
-                <div className="test2">
-                    <h1>Skills</h1>
-                    <h3>Education | Skills</h3>
+                <div id="skillHeader">
+                    <h1>My Skills</h1>
+                    {/* <div id="divider"> </div> */}
+                </div>
+                <div id="skillContainer">
+                    <div className="skills">
+                        <img className="svg" src={FrontEnd} alt={FrontEnd} />
+                        <h4>Front-End Development</h4>
+                        <p>Web Design tools such as HTML, CSS,SCSS, Javascript and React.js</p>
+                    </div>
+                    <div className="skills">
+                        <img className="svg" src={BackEnd} alt={BackEnd} />
+                        <h4>Back-End Development</h4>
+                        <p>Server side tools such as Express, Node.js, MongoDB, SQL, NoSQL and OOP </p>
+                    </div>
+                    <div className="skills">
+                        <img className="svg" src={Tools} alt={Tools} />
+                        <h4>Web Dev Tools</h4>
+                        <p>Git, GitBash, GitHub and VS Code </p>
+                    </div>   
+                    
                 </div>
             </section>
         </div>
