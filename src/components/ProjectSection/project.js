fbc9cee329b9553c7cfde691b56135c6cf119637
Noah8863 <stacknoah99@gmail.com>
2022-09-12 17:26:11 -0600

    added description and links to projects

--- src/components/ProjectSection/project.js
+++ src/components/ProjectSection/project.js
@@ -17,11 +17,11 @@ function Projects() {
                                     <img className="card__thumb" src={MoovySpace} alt="" />
                                     <div className="card__header-text">
                                         <h3 className="card__title">MoovySpace</h3>
-                                        <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>
+                                        <span className="card__tagline">Live Link: <a href="https://moovyspace.herokuapp.com/" target="_blank">MoovySpace</a></span>
                                         <span className="card__status">Completed: August 6th 2022</span>
                                     </div>
                                 </div>
-                                <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                                <p className="card__description">MoovySpace is a full stack application that allows users to find movie details, add movies to their 'Watch Later' or 'Liked Movies' section and view other users profile page</p>
                             </div>
                         </a>
                     </li>
@@ -33,11 +33,11 @@ function Projects() {
                                     <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
                                     <div className="card__header-text">
                                         <h3 className="card__title">Colorado Building Solutions</h3>
-                                        <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>
+                                        <span className="card__tagline">Live Link: <a href="http://coloradobuildingsolutions.com/" target="_blank">Colorado Building Solutions</a></span>
                                         <span className="card__status">Completed: August 22nd 2022</span>
                                     </div>
                                 </div>
-                                <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                                <p className="card__description">Colorado Building Solutions is a front-end eCommerce website that sells lumber and offers roofing service.</p>
                             </div>
                         </a>
                     </li>
@@ -49,8 +49,24 @@ function Projects() {
                                     <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
                                     <div className="card__header-text">
                                         <h3 className="card__title">Coffee and Book Finder</h3>
-                                        <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>
+                                        <span className="card__tagline">Live Link: <a href="https://Noah8863.github.io/Coffee-and-Book-Finder/" target="_blank">Coffee and Book Finder</a></span>
                                         <span className="card__status">Completed: April 9th 2022</span>
+                                    </div>
+                                </div>
+                                <p className="card__description">Coffee and Book Finder is a front-end only application that allows users to find book details and close by coffee shops using varies API's</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li id="project1">
+                        <a href="#project1" className="card">
+                            <img src={MoovySpace} className="card__image" alt="" />
+                            <div className="card__overlay">
+                                <div className="card__header">
+                                    <img className="card__thumb" src={MoovySpace} alt="" />
+                                    <div className="card__header-text">
+                                        <h3 className="card__title">Project Awesome TBA</h3>
+                                        <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>
+                                        <span className="card__status">Completed: August 6th 2022</span>
                                     </div>
                                 </div>
                                 <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
