dceff0392d8a6834deb00a219bc9d687ad521010
Noah8863 <stacknoah99@gmail.com>
2022-09-12 16:42:11 -0600

    added awesome floating shape and started the redesign for projects

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './hero.scss';
+import { useRef } from "react";
+import { useAnimationFrame } from "framer-motion";
 
-function Hero(){
+function Hero() {
+    const ref = useRef(null);
+
+    useAnimationFrame((t) => {
+        const rotate = Math.sin(t / 10000) * 200;
+        const y = (1 + Math.sin(t / 1000)) * -50;
+        ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`;
+    });
+
     return (
-       
         <section id="heroContainer">
-            <div className="test1">
-                <h1>Hello, my name is Noah Hoffman</h1>
-                <p>Anything in this container will not be effected by the navbar</p>
+        <div className="test1">
+            <h1>Hello, my name is Noah Hoffman</h1>
+            <p>Anything in this container will not be effected by the navbar</p>
+        </div>
+        <div className="shapes">
+            <div className="cube" ref={ref}>
+                <div className="side front" />
+                <div className="side left" />
+                <div className="side right" />
+                <div className="side top" />
+                <div className="side bottom" />
+                <div className="side back" />
             </div>
+        </div>
         </section>
-    )  
+    );
 }
 
 export default Hero;
--- src/components/ProjectSection/project.js
+++ src/components/ProjectSection/project.js
@@ -1,87 +1,62 @@
 import React from 'react';
 import './project.scss'
-
-import project1 from '../../Images/logo.jpg';
-import project2 from '../../Images/Book-Coffee-Finder.png';
-import project3 from '../../Images/MoovySpace.png'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import MoovySpace from '../../Images/MoovySpace.png';
+import CBS from '../../Images/CBS.jpg';
+import Coffee from '../../Images/Book-Coffee-Finder.png'
 
 function Projects() {
-    const handleBackground = (e) => {
-        const background = document.getElementById('projects');
-        if (e.target.id === 'item1') {
-            background.classList.remove('coffeeBackDrop')
-            background.classList.remove('passwordBackDrop')
-            background.classList.add('carBackDrop')
-        }
-        else if (e.target.id === 'item2') {
-            background.classList.remove('carBackDrop')
-            background.classList.remove('passwordBackDrop')
-            background.classList.add('coffeeBackDrop')
-        }
-        else if (e.target.id === 'item3') {
-            background.classList.remove('carBackDrop')
-            background.classList.remove('coffeeBackDrop')
-            background.classList.add('passwordBackDrop')
-        }
-    }
-
     return (
         <div className="projectContainer">
             <section id="projectSection">
-                <div className="ProjectCarousel">
-                    <input type="radio" name="slider" id="item1" onClick={handleBackground} defaultChecked></input>
-                    <input type="radio" name="slider" id="item2" onClick={handleBackground} defaultChecked></input>
-                    <input type="radio" name="slider" id="item3" onClick={handleBackground} defaultChecked></input>
-                    <div className="cards">
-                        <label className="card" htmlFor="item1" id="project1">
-                            <img
-                                src={project1} alt={project1}></img>
-                        </label>
-                        <label className="card" htmlFor="item2" id="project2">
-                            <img
-                                src={project2} alt={project2}></img>
-                        </label>
-                        <label className="card" htmlFor="item3" id="project3">
-                            <img
-                                src={project3} alt={project3}></img>
-                        </label>
-                    </div>
-                    <div className="infoContainer" id="infoContainer">
-                        <div className="upperpart">
-                            <div className="infoarea" id="test">
-                                <label className="projectinfo" id="projectinfo1">
-                                    <div className="title">Colorado Building Solutions</div>
-                                    <div className="subline">
-                                        <div className="liveLink"><a href="www.http://coloradobuildingsolutions.com/"
-                                            target="_blank" rel="noreferrer">Live Link Here</a></div>
-                                        <a className="githubIcon" href="https://github.com/Noah8863/COBuildingSolutions" target="_blank" rel="noreferrer">Source Code <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+                <ul className="cards">
+                    <li>
+                        <a href="#projectSection" className="card">
+                            <img src={MoovySpace} className="card__image" alt="" />
+                            <div className="card__overlay">
+                                <div className="card__header">
+                                    
+                                    <img className="card__thumb" src={MoovySpace} alt="" />
+                                    <div className="card__header-text">
+                                        <h3 className="card__title">MoovySpace</h3>
+                                        <span className="card__status">1 hour ago</span>
                                     </div>
-                                </label>
-                                <label className="projectinfo" id="projectinfo2">
-                                    <div className="title">Coffee and Book Finder</div>
-                                    <div className="subline">
-                                        <div className="liveLink"><a href="https://noah8863.github.io/Coffee-and-Book-Finder/"
-                                            target="_blank" rel="noreferrer">Live Link Here</a></div>
-                                        <a className="githubIcon" href="https://github.com/Noah8863/Coffee-and-Book-Finder"
-                                            target="_blank" rel="noreferrer">Source Code <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
-                                    </div>
-                                </label>
-                                <label className="projectinfo" id="projectinfo3">
-                                    <div className="title">MoovySpace</div>
-                                    <div className="subline">
-                                        <div className="liveLink"><a href="https://moovyspace.herokuapp.com/"
-                                            target="_blank" rel="noreferrer">Live Link Here</a></div>
-                                        <a className="githubIcon" href="https://github.com/jgault87/MoovySpace"
-                                            target="_blank" rel="noreferrer">Source Code <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
-                                    </div>
-                                </label>
+                                </div>
+                                <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
                             </div>
-                        </div>
-                    </div>
-                </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" className="card">
+                            <img src={CBS} className="card__image" alt="" />
+                            <div className="card__overlay">
+                                <div className="card__header">
+                                    <img className="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+                                    <div className="card__header-text">
+                                        <h3 className="card__title">Colorado Building Solutions</h3>
+                                        <span className="card__status">3 hours ago</span>
+                                    </div>
+                                </div>
+                                <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" className="card">
+                            <img src={Coffee}className="card__image" alt="" />
+                            <div className="card__overlay">
+                                <div className="card__header">
+                                    <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                                    <div className="card__header-text">
+                                        <h3 className="card__title">Coffee and Book Finder</h3>
+                                        <span className="card__tagline">Lorem ipsum dolor sit amet consectetur</span>
+                                        <span className="card__status">1 hour ago</span>
+                                    </div>
+                                </div>
+                                <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </section>
         </div>
     )
