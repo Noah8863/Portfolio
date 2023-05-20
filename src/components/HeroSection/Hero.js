10f7915be07313e4bcf9d661a71f76c867cede1e
Noah Hoffman <stacknoah99@gmail.com>
2023-05-20 14:53:20 -0600

    finally figured out the rotation/orbit speed

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -55,7 +55,7 @@ function Hero() {
               </div>
             </div>
           </div>
-          <div className="invisibleContainer"></div>
+          {/* <div className="invisibleContainer"></div> */}
           {/* <p id="hero-quote">Center of My Creations</p> */}
       </div>
     </section>
